56baa5ebfe111e33d91452881ce1c0f059957556
germanbelloni <gerbelloni@gmail.com>
2024-06-04 19:19:50 -0300

    tercero

--- server.js
+++ server.js
@@ -16,26 +16,45 @@ app.get("/", (req, res) => {
   res.send("Bienvenido a la API de frutas!");
 });
 
-app.get('/frutas', async (req,res) =>{
-    const client = await connectToMongoDB()
-    if (!client){
-        res.status(500).send('Error al conectarse a la BD')
-    }
+app.get("/frutas", async (req, res) => {
+  const client = await connectToMongoDB();
+  if (!client) {
+    res.status(500).send("Error al conectarse a la BD");
+  }
 
-    try {
-        const db = client.db('frutas')
-        const frutas = await db.collection('frutas').find().toArray()
+  try {
+    const db = client.db("frutas");
+    const frutas = await db.collection("frutas").find().toArray();
 
-        res.json(frutas)
-        
-    } catch (error) {
-        res.status(500).send('Error al obtener las frutas')
+    res.json(frutas);
+  } catch (error) {
+    res.status(500).send("Error al obtener las frutas");
+  } finally {
+    await disconnectFromMongoDB();
+  }
+});
+
+app.get("/frutas/:id", async (req, res) => {
+  const client = await connectToMongoDB();
+  if (!client) {
+    res.status(500).send("Error al conectarse a la BD");
+  }
+
+  try {
+    const frutaID = parseInt(req.params.id);
+    const db = client.db("frutas");
+    const fruta = await db.collection("frutas").findOne({ id: frutaID });
+    if (fruta){
+        res.json(fruta)
+    }else{
+res.status(404).send(`No se encontro la fruta con id ${frutaID}`)
     }
-    finally{
-        await disconnectFromMongoDB()
-    }
-    
-})
+  } catch (error) {
+    res.status(500).send("Error al obtener la fruta");
+  } finally {
+    await disconnectFromMongoDB();
+  }
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost:${port}`);
--- src/mongodb.js
+++ src/mongodb.js
@@ -1,31 +1,30 @@
-require('dotenv').config(); 
+require("dotenv").config();
 
-
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require("mongodb");
 
 //obtenemos la uri desde las variables de entorno
-const URI = process.env.MONGODB_URLSTRING
+const URI = process.env.MONGODB_URLSTRING;
 //hacemos la conexion del cliente
-const client = new MongoClient(URI)
+const client = new MongoClient(URI);
 
-async function connectToMongoDB(){
-    try {
-        await client.connect()
-        console.log('Conectadose a MongoDB')
-        return client
-    } catch (error) {
-        console.error('Error al conectar a MongoDB', error)
-        return null
-    }
+async function connectToMongoDB() {
+  try {
+    await client.connect();
+    console.log("Conectadose a MongoDB");
+    return client;
+  } catch (error) {
+    console.error("Error al conectar a MongoDB", error);
+    return null;
+  }
 }
 
-async function disconnectFromMongoDB(){
-    try {
-        await client.close()
-        console.log('Desconectadose de MongoDB')
-    } catch (error) {
-        console.error('Error al desconectar MongoDB', error)
-    }
+async function disconnectFromMongoDB() {
+  try {
+    await client.close();
+    console.log("Desconectadose de MongoDB");
+  } catch (error) {
+    console.error("Error al desconectar MongoDB", error);
+  }
 }
 
-module.exports = { connectToMongoDB, disconnectFromMongoDB }
+module.exports = { connectToMongoDB, disconnectFromMongoDB };
